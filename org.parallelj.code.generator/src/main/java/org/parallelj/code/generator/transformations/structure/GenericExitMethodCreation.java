664733357a072429bf6d2ddffb02918fd5c3cfb5
tnachtergaele <nachtergaele.thomas@gmail.com>
2015-06-30 17:39:01 +0200

    Rewrote transformations on entry and exit methods in pipeline creation process

--- org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/GenericExitMethodCreation.java
+++ org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/GenericExitMethodCreation.java
@@ -2,20 +2,17 @@ package org.parallelj.code.generator.transformations.structure;
 
 import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
 import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
+import net.atos.optimus.m2m.engine.ctxinject.api.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.ctxinject.api.ObjectContextElement;
+import net.atos.optimus.m2m.engine.ctxinject.api.ParentContextElement;
 import net.atos.optimus.m2m.javaxmi.core.annotations.GeneratedAnnotationAdder;
-import net.atos.optimus.m2m.javaxmi.core.javadoc.JavadocHelper;
+import net.atos.optimus.m2m.javaxmi.operation.classes.JavaClass;
+import net.atos.optimus.m2m.javaxmi.operation.instruction.complex.ComplexInstruction;
+import net.atos.optimus.m2m.javaxmi.operation.methods.Method;
+import net.atos.optimus.m2m.javaxmi.operation.methods.MethodHelper;
 
-import org.eclipse.gmt.modisco.java.AbstractMethodDeclaration;
-import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-import org.eclipse.gmt.modisco.java.Block;
-import org.eclipse.gmt.modisco.java.LineComment;
-import org.eclipse.gmt.modisco.java.Modifier;
-import org.eclipse.gmt.modisco.java.ReturnStatement;
-import org.eclipse.gmt.modisco.java.SingleVariableDeclaration;
-import org.eclipse.gmt.modisco.java.Type;
-import org.eclipse.gmt.modisco.java.TypeAccess;
-import org.eclipse.gmt.modisco.java.VisibilityKind;
-import org.eclipse.gmt.modisco.java.emf.JavaFactory;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
+import org.eclipse.gmt.modisco.java.MethodDeclaration;
 import org.parallelj.code.generator.core.Messages;
 import org.parallelj.model.Procedure;
 
@@ -26,90 +23,41 @@ import org.parallelj.model.Procedure;
  * @version 1.0
  * 
  */
-public class GenericExitMethodCreation extends
-		AbstractTransformation<Procedure> {
+public class GenericExitMethodCreation extends AbstractTransformation<Procedure> {
+
+	@ParentContextElement(value = "self", nullable = false)
+	private ClassDeclaration parent;
+
+	@ObjectContextElement(value = "exit", visibility = ContextElementVisibility.OUT, nullable = false)
+	private MethodDeclaration exitMethod;
 
 	// if procedure is from pipeline
 	private boolean isPipelineProcedure;
 
-	public GenericExitMethodCreation(Procedure eObject, String id,
-			boolean isForPipeline) {
+	public GenericExitMethodCreation(Procedure eObject, String id, boolean isForPipeline) {
 		super(eObject, id);
 		this.isPipelineProcedure = isForPipeline;
 	}
 
 	@Override
 	protected void transform(ITransformationContext context) {
-		Procedure procedure = getEObject();
-
-		AbstractMethodDeclaration exitMethod = JavaFactory.eINSTANCE
-				.createMethodDeclaration();
-		exitMethod.setName(procedure.getName());
-
-		AbstractTypeDeclaration parent = (AbstractTypeDeclaration) context.get(
-				getEObject().eContainer(), "self");
-		exitMethod.setOriginalCompilationUnit(parent
-				.getOriginalCompilationUnit());
-		exitMethod.setAbstractTypeDeclaration(parent);
-
-		Modifier modifier = JavaFactory.eINSTANCE.createModifier();
-		modifier.setVisibility(VisibilityKind.PUBLIC);
-		modifier.setOriginalCompilationUnit(parent.getOriginalCompilationUnit());
-		exitMethod.setModifier(modifier);
-
-		// if it is pipeline procedure, then it will have one parameter as
-		// object in method
+		Method method = MethodHelper
+				.builder(new JavaClass(this.parent), getEObject().getName())
+				.build()
+				.addInstructions(
+						ComplexInstruction.createEmptyReturnInstruction().addComment(Messages.COMMENT_TODO.message(),
+								true));
 		if (isPipelineProcedure) {
-			SingleVariableDeclaration declaration = JavaFactory.eINSTANCE
-					.createSingleVariableDeclaration();
-			declaration.setMethodDeclaration(exitMethod);
-			declaration.setModifier(JavaFactory.eINSTANCE.createModifier());
-			declaration.setVarargs(false);
-
-			TypeAccess parameterTypeAccess = JavaFactory.eINSTANCE
-					.createTypeAccess();
-			Type parameterType = JavaFactory.eINSTANCE.createPrimitiveType();
-			parameterType.setName("Object");
-			parameterTypeAccess.setType(parameterType);
-
-			declaration.setType(parameterTypeAccess);
-			declaration.setName("next");
-			declaration.setOriginalCompilationUnit(parent
-					.getOriginalCompilationUnit());
+			method.addParameter("Object", "next").addJavadoc(
+					Messages.JAVADOC_PIPELINE_EXIT_METHOD.message(getEObject().getName(),
+							getEObject().getExecutable() != null ? getEObject().getExecutable() : "", getEObject()
+									.getDescription() != null ? getEObject().getDescription() : ""), true);
+		} else {
+			method.addJavadoc(Messages.JAVADOC_PROCEDURE_EXIT_METHOD.message(getEObject().getName(), getEObject()
+					.getExecutable() != null ? getEObject().getExecutable() : "",
+					getEObject().getDescription() != null ? getEObject().getDescription() : ""), true);
 		}
-
-		LineComment lineComment = JavaFactory.eINSTANCE.createLineComment();
-		lineComment.setContent(Messages.COMMENT_TODO.message());
-		lineComment.setPrefixOfParent(true);
-
-		ReturnStatement returnStatement = JavaFactory.eINSTANCE
-				.createReturnStatement();
-		returnStatement.getComments().add(lineComment);
-
-		Block block = JavaFactory.eINSTANCE.createBlock();
-		block.getStatements().add(returnStatement);
-		exitMethod.setBody(block);
-
-		if (!isPipelineProcedure) {
-		JavadocHelper.addJavadoc(exitMethod,
-				Messages.JAVADOC_PROCEDURE_EXIT_METHOD.message(getEObject()
-						.getName(),
-						getEObject().getExecutable() != null ? getEObject()
-								.getExecutable() : "", getEObject()
-								.getDescription() != null ? getEObject()
-								.getDescription() : ""));
-		}else{
-			JavadocHelper.addJavadoc(exitMethod,
-					Messages.JAVADOC_PIPELINE_EXIT_METHOD.message(getEObject()
-							.getName(),
-							getEObject().getExecutable() != null ? getEObject()
-									.getExecutable() : "", getEObject()
-									.getDescription() != null ? getEObject()
-									.getDescription() : ""));
-		}
-
+		this.exitMethod = method.getDelegate();
 		GeneratedAnnotationAdder.addGenerated(exitMethod, "//J", true, false);
-
-		context.put(procedure, "exit", exitMethod);
 	}
 }
--- org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/ProcedureEntryMethodCreation.java
+++ org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/ProcedureEntryMethodCreation.java
@@ -2,22 +2,17 @@ package org.parallelj.code.generator.transformations.structure;
 
 import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
 import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
+import net.atos.optimus.m2m.engine.ctxinject.api.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.ctxinject.api.ObjectContextElement;
+import net.atos.optimus.m2m.engine.ctxinject.api.ParentContextElement;
 import net.atos.optimus.m2m.javaxmi.core.annotations.GeneratedAnnotationAdder;
-import net.atos.optimus.m2m.javaxmi.core.javadoc.JavadocHelper;
+import net.atos.optimus.m2m.javaxmi.operation.classes.JavaClass;
+import net.atos.optimus.m2m.javaxmi.operation.instruction.CallInstructionHelper;
+import net.atos.optimus.m2m.javaxmi.operation.methods.Method;
+import net.atos.optimus.m2m.javaxmi.operation.methods.MethodHelper;
 
-import org.eclipse.gmt.modisco.java.AbstractMethodDeclaration;
-import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-import org.eclipse.gmt.modisco.java.Block;
-import org.eclipse.gmt.modisco.java.ClassInstanceCreation;
-import org.eclipse.gmt.modisco.java.LineComment;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
 import org.eclipse.gmt.modisco.java.MethodDeclaration;
-import org.eclipse.gmt.modisco.java.Modifier;
-import org.eclipse.gmt.modisco.java.ReturnStatement;
-import org.eclipse.gmt.modisco.java.SingleVariableDeclaration;
-import org.eclipse.gmt.modisco.java.Type;
-import org.eclipse.gmt.modisco.java.TypeAccess;
-import org.eclipse.gmt.modisco.java.VisibilityKind;
-import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.parallelj.code.generator.core.Messages;
 import org.parallelj.model.Procedure;
 
@@ -29,14 +24,18 @@ import org.parallelj.model.Procedure;
  * @version 1.0
  * 
  */
-public class ProcedureEntryMethodCreation extends
-		AbstractTransformation<Procedure> {
+public class ProcedureEntryMethodCreation extends AbstractTransformation<Procedure> {
+
+	@ParentContextElement(value = "self", nullable = false)
+	private ClassDeclaration parent;
+
+	@ObjectContextElement(value = "self", visibility = ContextElementVisibility.OUT, nullable = false)
+	private MethodDeclaration entryMethod;
 
 	// if putting entry method for pipeline
 	private boolean isForPipeline;
 
-	public ProcedureEntryMethodCreation(Procedure eObject, String id,
-			boolean isForPipeline) {
+	public ProcedureEntryMethodCreation(Procedure eObject, String id, boolean isForPipeline) {
 		super(eObject, id);
 		this.isForPipeline = isForPipeline;
 	}
@@ -44,82 +43,22 @@ public class ProcedureEntryMethodCreation extends
 	@Override
 	protected void transform(ITransformationContext context) {
 		Procedure procedure = getEObject();
-
-		AbstractMethodDeclaration entryMethod = JavaFactory.eINSTANCE
-				.createMethodDeclaration();
-		entryMethod.setName(getEObject().getName());
-
-		AbstractTypeDeclaration parent = (AbstractTypeDeclaration) context.get(
-				getEObject().eContainer(), "self");
-		entryMethod.setOriginalCompilationUnit(parent
-				.getOriginalCompilationUnit());
-		entryMethod.setAbstractTypeDeclaration(parent);
-
-		Modifier modifier = JavaFactory.eINSTANCE.createModifier();
-		modifier.setVisibility(VisibilityKind.PUBLIC);
-		modifier.setOriginalCompilationUnit(parent.getOriginalCompilationUnit());
-		entryMethod.setModifier(modifier);
-
-		MethodDeclaration methodDeclaration = (MethodDeclaration) entryMethod;
-
-		// if it is pipeline procedure, then it will have one parameter as
-		// object in method
+		Method method = MethodHelper
+				.builder(new JavaClass(this.parent), procedure.getName())
+				.setReturnType(procedure.getExecutable())
+				.addInstructions(
+						CallInstructionHelper.createClassInstanciationInstruction(procedure.getExecutable())
+								.convertToReturnInstruction().addComment(Messages.COMMENT_TODO.message(), true))
+				.build()
+				.addJavadoc(
+						Messages.JAVADOC_PROCEDURE_ENTRY_METHOD.message(getEObject().getName(), getEObject()
+								.getExecutable(), (getEObject().getDescription() != null ? getEObject()
+								.getDescription() : "")), true);
 		if (isForPipeline) {
-			SingleVariableDeclaration declaration = JavaFactory.eINSTANCE
-					.createSingleVariableDeclaration();
-			declaration.setMethodDeclaration(entryMethod);
-			declaration.setModifier(JavaFactory.eINSTANCE.createModifier());
-			declaration.setVarargs(false);
-
-			TypeAccess parameterTypeAccess = JavaFactory.eINSTANCE
-					.createTypeAccess();
-			Type parameterType = JavaFactory.eINSTANCE.createPrimitiveType();
-			parameterType.setName("Object");
-			parameterTypeAccess.setType(parameterType);
-
-			declaration.setType(parameterTypeAccess);
-			declaration.setName("next");
-			declaration.setOriginalCompilationUnit(parent
-					.getOriginalCompilationUnit());
+			method.addParameter("Object", "next");
 		}
-
-		TypeAccess createTypeAccess = JavaFactory.eINSTANCE.createTypeAccess();
-		Type type = JavaFactory.eINSTANCE.createClassDeclaration();
-		type.setName(procedure.getExecutable());
-		createTypeAccess.setType(type);
-		methodDeclaration.setReturnType(createTypeAccess);
-
-		ReturnStatement returnStatement = JavaFactory.eINSTANCE
-				.createReturnStatement();
-		ClassInstanceCreation returnSta = JavaFactory.eINSTANCE
-				.createClassInstanceCreation();
-		createTypeAccess = JavaFactory.eINSTANCE.createTypeAccess();
-		Type classType = JavaFactory.eINSTANCE
-				.createUnresolvedClassDeclaration();
-
-		classType.setName(getEObject().getExecutable());
-		createTypeAccess.setType(classType);
-		returnSta.setType(createTypeAccess);
-		returnStatement.setExpression(returnSta);
-
-		LineComment lineComment = JavaFactory.eINSTANCE.createLineComment();
-		lineComment.setContent(Messages.COMMENT_TODO.message());
-		lineComment.setPrefixOfParent(true);
-		returnStatement.getComments().add(lineComment);
-
-		Block block = JavaFactory.eINSTANCE.createBlock();
-		block.getStatements().add(returnStatement);
-		entryMethod.setBody(block);
-
-		JavadocHelper.addJavadoc(entryMethod,
-				Messages.JAVADOC_PROCEDURE_ENTRY_METHOD.message(getEObject()
-						.getName(), getEObject().getExecutable(), (getEObject()
-						.getDescription() != null ? getEObject()
-						.getDescription() : "")));
-
+		this.entryMethod = method.getDelegate();
 		GeneratedAnnotationAdder.addGenerated(entryMethod, "//J", true, false);
-
-		context.put(procedure, "self", entryMethod);
 	}
 
 }
