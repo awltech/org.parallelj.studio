e692983d4f21d4ded60308fc09c7793a5111a41c
tnachtergaele <nachtergaele.thomas@gmail.com>
2015-07-01 16:42:47 +0200

    Rewrote pipeline entry method and parameter annotation creation

--- org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/flow/PipelineParameterAnnotationCreation.java
+++ org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/flow/PipelineParameterAnnotationCreation.java
@@ -2,28 +2,33 @@ package org.parallelj.code.generator.transformations.flow;
 
 import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
 import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
-import net.atos.optimus.m2m.javaxmi.core.annotations.JavaAnnotationHelper;
+import net.atos.optimus.m2m.engine.ctxinject.api.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.ctxinject.api.ObjectContextElement;
+import net.atos.optimus.m2m.engine.ctxinject.api.ParentContextElement;
+import net.atos.optimus.m2m.javaxmi.operation.annotations.AnnotationHelper;
+import net.atos.optimus.m2m.javaxmi.operation.methods.Method;
+import net.atos.optimus.m2m.javaxmi.operation.parameters.ParameterHelper;
 
-import org.eclipse.gmt.modisco.java.AbstractMethodDeclaration;
-import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-import org.eclipse.gmt.modisco.java.Annotation;
-import org.eclipse.gmt.modisco.java.SingleVariableDeclaration;
-import org.eclipse.gmt.modisco.java.Type;
-import org.eclipse.gmt.modisco.java.TypeAccess;
-import org.eclipse.gmt.modisco.java.emf.JavaFactory;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
+import org.eclipse.gmt.modisco.java.MethodDeclaration;
 import org.parallelj.model.Pipeline;
 
 /**
- * This transformation allows to add the <code>@PipelineParameter</code> annotation and
- * the method parameter on an entry method. It also manages the parameters of
- * the annotation
+ * This transformation allows to add the <code>@PipelineParameter</code>
+ * annotation and the method parameter on an entry method. It also manages the
+ * parameters of the annotation
  * 
  * @author a169104
  * @version 1.0
  * 
  */
-public class PipelineParameterAnnotationCreation extends
-		AbstractTransformation<Pipeline> {
+public class PipelineParameterAnnotationCreation extends AbstractTransformation<Pipeline> {
+
+	@ParentContextElement(value = "self", nullable = false)
+	private ClassDeclaration parent;
+
+	@ObjectContextElement(value = "entry", visibility = ContextElementVisibility.INOUT, nullable = false)
+	private MethodDeclaration declaration;
 
 	public PipelineParameterAnnotationCreation(Pipeline eObject, String id) {
 		super(eObject, id);
@@ -32,38 +37,17 @@ public class PipelineParameterAnnotationCreation extends
 	@Override
 	protected void transform(ITransformationContext context) {
 		Pipeline pipeline = getEObject();
-		AbstractMethodDeclaration declaration = (AbstractMethodDeclaration) context
-				.get(pipeline, "entry");
-
-		AbstractTypeDeclaration parent = (AbstractTypeDeclaration) context.get(
-				getEObject().eContainer(), "self");
-
-		SingleVariableDeclaration variable = JavaFactory.eINSTANCE
-				.createSingleVariableDeclaration();
-		variable.setMethodDeclaration(declaration);
-		variable.setModifier(JavaFactory.eINSTANCE.createModifier());
-		variable.setVarargs(false);
-
-		TypeAccess parameterTypeAccess = JavaFactory.eINSTANCE
-				.createTypeAccess();
-		Type parameterType = JavaFactory.eINSTANCE.createPrimitiveType();
-		parameterType.setName(pipeline.getIterable().getType());
-		parameterTypeAccess.setType(parameterType);
-
-		variable.setType(parameterTypeAccess);
-		variable.setName(pipeline.getIterable().getName());
-
-		Annotation annotation = JavaAnnotationHelper.addAnnotation(
-				variable.getMethodDeclaration(), "org.parallelj", "PipelineParameter");
-
-		JavaAnnotationHelper.addAnnotationParameter(annotation, "value",
-				pipeline.getIterable() != null ? pipeline.getIterable().getName()
-						: "");
-
-		variable.getAnnotations().add(annotation);
-		variable.setOriginalCompilationUnit(parent.getOriginalCompilationUnit());
-
-		context.put(pipeline, "self", declaration);
+		Method method = new Method(this.declaration).addParameters(ParameterHelper
+				.builder(pipeline.getIterable().getType())
+				.setName(pipeline.getIterable().getName())
+				.build()
+				.addAnnotations(
+						AnnotationHelper
+								.builder("org.parallelj", "PipelineParameter")
+								.addAnnotationParameter("value",
+										pipeline.getIterable() != null ? pipeline.getIterable().getName() : "", true)
+								.build()));
+		this.declaration = method.getDelegate();
 	}
 
 }
--- org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/PipelineEntryMethodCreation.java
+++ org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/PipelineEntryMethodCreation.java
@@ -2,23 +2,16 @@ package org.parallelj.code.generator.transformations.structure;
 
 import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
 import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
+import net.atos.optimus.m2m.engine.ctxinject.api.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.ctxinject.api.ObjectContextElement;
+import net.atos.optimus.m2m.engine.ctxinject.api.ParentContextElement;
 import net.atos.optimus.m2m.javaxmi.core.annotations.GeneratedAnnotationAdder;
-import net.atos.optimus.m2m.javaxmi.core.javadoc.JavadocHelper;
+import net.atos.optimus.m2m.javaxmi.operation.classes.JavaClass;
+import net.atos.optimus.m2m.javaxmi.operation.instruction.CallInstructionHelper;
+import net.atos.optimus.m2m.javaxmi.operation.methods.MethodHelper;
 
-import org.eclipse.gmt.modisco.java.AbstractMethodDeclaration;
-import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-import org.eclipse.gmt.modisco.java.Block;
-import org.eclipse.gmt.modisco.java.ClassInstanceCreation;
-import org.eclipse.gmt.modisco.java.LineComment;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
 import org.eclipse.gmt.modisco.java.MethodDeclaration;
-import org.eclipse.gmt.modisco.java.Modifier;
-import org.eclipse.gmt.modisco.java.ReturnStatement;
-import org.eclipse.gmt.modisco.java.SingleVariableAccess;
-import org.eclipse.gmt.modisco.java.Type;
-import org.eclipse.gmt.modisco.java.TypeAccess;
-import org.eclipse.gmt.modisco.java.VariableDeclarationFragment;
-import org.eclipse.gmt.modisco.java.VisibilityKind;
-import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.parallelj.code.generator.core.Messages;
 import org.parallelj.code.generator.helpers.StringFormatHelper;
 import org.parallelj.model.Pipeline;
@@ -31,8 +24,14 @@ import org.parallelj.model.Pipeline;
  * @version 1.0
  * 
  */
-public class PipelineEntryMethodCreation extends
-		AbstractTransformation<Pipeline> {
+public class PipelineEntryMethodCreation extends AbstractTransformation<Pipeline> {
+
+	 @ParentContextElement(value = "self", nullable = false)
+	 private ClassDeclaration parent;
+	
+	 @ObjectContextElement(value = "entry", visibility =
+	 ContextElementVisibility.OUT, nullable = false)
+	 private MethodDeclaration entryMethod;
 
 	public PipelineEntryMethodCreation(Pipeline eObject, String id) {
 		super(eObject, id);
@@ -41,76 +40,21 @@ public class PipelineEntryMethodCreation extends
 	@Override
 	protected void transform(ITransformationContext context) {
 		Pipeline pipeline = getEObject();
-
-		AbstractMethodDeclaration entryMethod = JavaFactory.eINSTANCE
-				.createMethodDeclaration();
-		entryMethod.setName(getEObject().getName());
-
-		AbstractTypeDeclaration parent = (AbstractTypeDeclaration) context.get(
-				getEObject().eContainer(), "self");
-		entryMethod.setOriginalCompilationUnit(parent
-				.getOriginalCompilationUnit());
-		entryMethod.setAbstractTypeDeclaration(parent);
-
-		Modifier modifier = JavaFactory.eINSTANCE.createModifier();
-		modifier.setVisibility(VisibilityKind.PUBLIC);
-		modifier.setOriginalCompilationUnit(parent.getOriginalCompilationUnit());
-		entryMethod.setModifier(modifier);
-
-		MethodDeclaration methodDeclaration = (MethodDeclaration) entryMethod;
-
-		TypeAccess createTypeAccess = JavaFactory.eINSTANCE.createTypeAccess();
-		Type type = JavaFactory.eINSTANCE.createClassDeclaration();
-		type.setName(StringFormatHelper.camelCase(pipeline.getName() + "Class",true));
-		createTypeAccess.setType(type);
-		methodDeclaration.setReturnType(createTypeAccess);
-
-		ReturnStatement returnStatement = JavaFactory.eINSTANCE
-				.createReturnStatement();
-		ClassInstanceCreation returnSta = JavaFactory.eINSTANCE
-				.createClassInstanceCreation();
-		createTypeAccess = JavaFactory.eINSTANCE.createTypeAccess();
-		Type classType = JavaFactory.eINSTANCE
-				.createUnresolvedClassDeclaration();
-
-		classType.setName(StringFormatHelper.camelCase(pipeline.getName() + "Class",true));
-		createTypeAccess.setType(classType);
-		returnSta.setType(createTypeAccess);
-		
-		// creating variable declaration
-		VariableDeclarationFragment variableDeclarationFragment = JavaFactory.eINSTANCE
-				.createVariableDeclarationFragment();
-
-		// adding name
-		variableDeclarationFragment.setName(pipeline.getIterable().getName());
-		variableDeclarationFragment.setProxy(false);
-
-		SingleVariableAccess singleVariableAccess = JavaFactory.eINSTANCE
-				.createSingleVariableAccess();
-		singleVariableAccess.setVariable(variableDeclarationFragment);
-		
-		returnSta.getArguments().add(singleVariableAccess);
-		
-		returnStatement.setExpression(returnSta);
-
-		LineComment lineComment = JavaFactory.eINSTANCE.createLineComment();
-		lineComment.setContent(Messages.COMMENT_TODO.message());
-		lineComment.setPrefixOfParent(true);
-		returnStatement.getComments().add(lineComment);
-
-		Block block = JavaFactory.eINSTANCE.createBlock();
-		block.getStatements().add(returnStatement);
-		entryMethod.setBody(block);
-
-		JavadocHelper.addJavadoc(entryMethod,
-				Messages.JAVADOC_PIPELINE_ENTRY_METHOD.message(getEObject()
-						.getName(), getEObject().getExecutable(), (getEObject()
-						.getDescription() != null ? getEObject()
-						.getDescription() : "")));
+		this.entryMethod = MethodHelper
+				.builder(new JavaClass(this.parent), pipeline.getName())
+				.setReturnType(StringFormatHelper.camelCase(pipeline.getName() + "Class", true))
+				.build()
+				.addInstructions(
+						CallInstructionHelper
+								.createClassInstanciationInstruction(
+										StringFormatHelper.camelCase(pipeline.getName() + "Class", true))
+								.addVariableArgument(pipeline.getIterable().getName()).convertToReturnInstruction()
+								.addComment(Messages.COMMENT_TODO.message(), true))
+				.addJavadoc(
+						Messages.JAVADOC_PIPELINE_ENTRY_METHOD.message(getEObject().getName(), getEObject()
+								.getExecutable(), (getEObject().getDescription() != null ? getEObject()
+								.getDescription() : "")), true).getDelegate();
 
 		GeneratedAnnotationAdder.addGenerated(entryMethod, "//J", true, false);
-
-		context.put(pipeline, "entry", entryMethod);
 	}
-
 }
