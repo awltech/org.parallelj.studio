e2c45aef2480251653f5b73163dcba400fbb97e4
tnachtergaele <nachtergaele.thomas@gmail.com>
2015-07-01 17:50:06 +0200

    Rewrote predicate creation

--- org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/PredicateCreation.java
+++ org.parallelj.code.generator/src/main/java/org/parallelj/code/generator/transformations/structure/PredicateCreation.java
@@ -2,21 +2,16 @@ package org.parallelj.code.generator.transformations.structure;
 
 import net.atos.optimus.m2m.engine.core.transformations.AbstractTransformation;
 import net.atos.optimus.m2m.engine.core.transformations.ITransformationContext;
+import net.atos.optimus.m2m.engine.ctxinject.api.ContextElementVisibility;
+import net.atos.optimus.m2m.engine.ctxinject.api.ObjectContextElement;
+import net.atos.optimus.m2m.engine.ctxinject.api.ParentContextElement;
 import net.atos.optimus.m2m.javaxmi.core.annotations.GeneratedAnnotationAdder;
-import net.atos.optimus.m2m.javaxmi.core.javadoc.JavadocHelper;
+import net.atos.optimus.m2m.javaxmi.operation.classes.JavaClass;
+import net.atos.optimus.m2m.javaxmi.operation.instruction.elementary.ElementaryInstructionHelper;
+import net.atos.optimus.m2m.javaxmi.operation.methods.MethodHelper;
 
-import org.eclipse.gmt.modisco.java.AbstractMethodDeclaration;
-import org.eclipse.gmt.modisco.java.AbstractTypeDeclaration;
-import org.eclipse.gmt.modisco.java.Block;
-import org.eclipse.gmt.modisco.java.BooleanLiteral;
-import org.eclipse.gmt.modisco.java.LineComment;
+import org.eclipse.gmt.modisco.java.ClassDeclaration;
 import org.eclipse.gmt.modisco.java.MethodDeclaration;
-import org.eclipse.gmt.modisco.java.Modifier;
-import org.eclipse.gmt.modisco.java.ReturnStatement;
-import org.eclipse.gmt.modisco.java.Type;
-import org.eclipse.gmt.modisco.java.TypeAccess;
-import org.eclipse.gmt.modisco.java.VisibilityKind;
-import org.eclipse.gmt.modisco.java.emf.JavaFactory;
 import org.parallelj.code.generator.core.Messages;
 import org.parallelj.model.Predicate;
 
@@ -29,6 +24,12 @@ import org.parallelj.model.Predicate;
  */
 public class PredicateCreation extends AbstractTransformation<Predicate> {
 
+	@ParentContextElement(value = "self", nullable = false)
+	private ClassDeclaration parent;
+
+	@ObjectContextElement(value = "self", visibility = ContextElementVisibility.OUT, nullable = false)
+	private MethodDeclaration declaration;
+
 	public PredicateCreation(Predicate eObject, String id) {
 		super(eObject, id);
 	}
@@ -36,56 +37,18 @@ public class PredicateCreation extends AbstractTransformation<Predicate> {
 	@Override
 	protected void transform(ITransformationContext context) {
 		Predicate predicate = getEObject();
-
-		AbstractMethodDeclaration declaration = JavaFactory.eINSTANCE
-				.createMethodDeclaration();
-
-		String predicateName = "is"
-				+ predicate.getName().substring(0, 1).toUpperCase()
-				+ predicate.getName().substring(1);
-		declaration.setName(predicateName);
-
-		AbstractTypeDeclaration parent = (AbstractTypeDeclaration) context.get(
-				getEObject().eContainer(), "self");
-		declaration.setOriginalCompilationUnit(parent
-				.getOriginalCompilationUnit());
-		declaration.setAbstractTypeDeclaration(parent);
-
-		Modifier modifier = JavaFactory.eINSTANCE.createModifier();
-		modifier.setVisibility(VisibilityKind.PUBLIC);
-		modifier.setOriginalCompilationUnit(parent.getOriginalCompilationUnit());
-		declaration.setModifier(modifier);
-
-		MethodDeclaration methodDeclaration = (MethodDeclaration) declaration;
-		TypeAccess returnTypeAccess = JavaFactory.eINSTANCE.createTypeAccess();
-		Type type = JavaFactory.eINSTANCE.createPrimitiveType();
-		type.setName("boolean");
-		returnTypeAccess.setType(type);
-		methodDeclaration.setReturnType(returnTypeAccess);
-
-		ReturnStatement returnStatement = JavaFactory.eINSTANCE
-				.createReturnStatement();
-		BooleanLiteral literal = JavaFactory.eINSTANCE.createBooleanLiteral();
-		literal.setValue(false);
-		returnStatement.setExpression(literal);
-
-		LineComment lineComment = JavaFactory.eINSTANCE.createLineComment();
-		lineComment.setContent(Messages.COMMENT_TODO.message());
-		lineComment.setPrefixOfParent(true);
-		returnStatement.getComments().add(lineComment);
-
-		Block block = JavaFactory.eINSTANCE.createBlock();
-		block.getStatements().add(returnStatement);
-		declaration.setBody(block);
-
-		JavadocHelper.addJavadoc(declaration, Messages.JAVADOC_PREDICATE
-				.message(getEObject().getName(),
-						(getEObject().getDescription() != null ? getEObject()
-								.getDescription() : "")));
-
+		this.declaration = MethodHelper
+				.builder(new JavaClass(this.parent),
+						"is" + predicate.getName().substring(0, 1).toUpperCase() + predicate.getName().substring(1))
+				.setReturnType("boolean")
+				.addInstructions(
+						ElementaryInstructionHelper.createInstruction(false).convertToReturnInstruction()
+								.addComment(Messages.COMMENT_TODO.message(), true))
+				.build()
+				.addJavadoc(
+						Messages.JAVADOC_PREDICATE.message(getEObject().getName(),
+								(getEObject().getDescription() != null ? getEObject().getDescription() : "")), true)
+				.getDelegate();
 		GeneratedAnnotationAdder.addGenerated(declaration, "//J", true, false);
-
-		context.put(predicate, "self", declaration);
 	}
-
 }
